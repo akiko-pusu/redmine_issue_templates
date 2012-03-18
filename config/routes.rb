64af02ca8cb8a36bc231ab46ed310331615e22b9
akiko_pusu <akiko.pusu@gmail.com>
2012-03-18 17:24:12 +0900

    Add routing for redmine trunk.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ ActionController::Routing::Routes.draw do |map|
 	map.connect 'projects/:project_id/issue_templates/:action/:id', :controller => 'issue_templates', :action => 'edit'
   map.connect 'projects/:project_id/issue_templates_settings/:action', 
     :controller => 'issue_templates_settings'
+  map.connect 'issue_templates/preview/:id', :controller => 'issue_templates', :action => 'preview'
+  map.connect 'projects/:project_id/issue_templates_settings/preview', :controller => 'issue_templates_settings', :action => 'preview', :conditions => {:method => :post }
 end
