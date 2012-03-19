b946f9c4a63af4548ce0b1b1e3396c545c060fb5
akiko_pusu <akiko.pusu@gmail.com>
2012-03-20 01:54:17 +0900

    Remove unused function.

--- app/helpers/issue_templates_helper.rb
+++ app/helpers/issue_templates_helper.rb
@@ -1,14 +1,2 @@
-module IssueTemplatesHelper
-  def grouped_array(trackers, project_id)
-    array = []
-    trackers.each {|tracker|
-      group = []
-      tmpls = IssueTemplate.find(:all, 
-        :conditions => ['tracker_id = ? AND project_id = ?',
-          item.id, project_id])
-      tmpls.each { |x| group.push([x.id, x.title]) }      
-      array.push([tracker.name, group])
-    }
-    return array
-  end  
+module IssueTemplatesHelper 
 end
