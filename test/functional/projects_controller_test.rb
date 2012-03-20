857c29cbd055972129595458026507f1d8a857ea
akiko_pusu <akiko.pusu@gmail.com>
2012-03-21 05:27:37 +0900

    Add permission to test user. (for test)

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -5,9 +5,6 @@ require 'projects_controller'
 class ProjectsController; def rescue_action(e) raise e end; end
 
 class ProjectsControllerTest < ActionController::TestCase
-#  fixtures :projects, :versions, :users, :roles, :members, :member_roles, :issues, :journals, :journal_details,
-#           :trackers, :projects_trackers, :issue_statuses, :enabled_modules, :enumerations, :boards, :messages,
-#           :attachments, :custom_fields, :custom_values, :time_entries
   fixtures :projects, :users, :roles, :members, :member_roles,
            :trackers, :projects_trackers, :enabled_modules
   def setup
@@ -16,17 +13,17 @@ class ProjectsControllerTest < ActionController::TestCase
     @response   = ActionController::TestResponse.new
     # as project admin
     @request.session[:user_id] = 2
+    Role.find(1).add_permission! :manage_issue_templates
     # Enabled Template module
-    enabled_module = EnabledModule.new
-    enabled_module.project_id = 1
-    enabled_module.name = 'issue_templates'	
-    enabled_module.save     
+    @project = Project.find(1)
+    @project.enabled_modules << EnabledModule.new(:name => 'issue_templates')
+    @project.save!    
   end
   
   def test_settings
     get :settings, :id => 1
     assert_response :success
     assert_template 'settings'
-    assert_select 'li a#tab-issue_templates'
+    assert_select 'a#tab-issue_templates'
   end  
 end
