efafe26f731954d0be97fde0855e7de0e819dabb
akiko_pusu <akiko.pusu@gmail.com>
2012-07-01 06:40:25 +0900

    Fixed routing bug.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   match 'projects/:project_id/issue_templates/:action', :to => 'issue_templates'
   match 'projects/:project_id/issue_templates/:action/:id', :to => 'issue_templates#edit'
   match 'projects/:project_id/issue_templates_settings/:action', :to => 'issue_templates_settings'
-  match 'issue_templates/preview/:id', :to => 'issue_templates#preview'
+  match 'issue_templates/preview', :to => 'issue_templates#preview', :via => [:get, :post]
   match 'projects/:project_id/issue_templates_settings/preview', :to => 'issue_templates_settings#preview', :via => [:get, :post]
 end
