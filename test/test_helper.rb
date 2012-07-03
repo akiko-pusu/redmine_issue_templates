08e0f34253814525a15264e88d5eba43a7b55955
akiko_pusu <akiko.pusu@gmail.com>
2012-07-03 11:17:32 +0900

    Change simplecov style.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
-require 'simplecov-rcov'
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+#require 'simplecov-rcov'
+#SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start 'rails'
 
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
