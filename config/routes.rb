e78f428c58dba774a2b232e2f86517adb427f128
akiko_pusu <akiko.pusu@gmail.com>
2012-07-21 20:50:46 +0900

    Update test code for reordering. (IssueID: #962)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do 
   match 'projects/:project_id/issue_templates/:action', :to => 'issue_templates'
   match 'projects/:project_id/issue_templates/:action/:id', :to => 'issue_templates#edit'
+  match 'projects/:project_id/issue_templates/move/:id', :to => 'issue_templates#move_to'
   match 'projects/:project_id/issue_templates_settings/:action', :to => 'issue_templates_settings'
   match 'issue_templates/preview', :to => 'issue_templates#preview', :via => [:get, :post]
   match 'projects/:project_id/issue_templates_settings/preview', :to => 'issue_templates_settings#preview', :via => [:get, :post]
