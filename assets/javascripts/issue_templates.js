c6c063aa267ec35f0a6ebf36e24269ecf1c85eef
akiko-pusu <akiko.pusu@gmail.com>
2012-11-28 08:34:25 +0900

    Change some parameters and enabled to load template. (IssueID: #1131)

--- assets/javascripts/issue_templates.js
+++ assets/javascripts/issue_templates.js
@@ -13,27 +13,26 @@ $(document).ready(function() {
     $('#template_area').insertBefore($('#issue_subject').parent());
 });
 
-function load_template(template, target_url, token) {
-    if (template != "") {
-        $.ajax({
-            url:target_url,
-            async:true,
-            data:'issue_template=' + encodeURIComponent(template) + '&authenticity_token='
-                + encodeURIComponent(token)
-        }).done(function (html) {
-                eval("var template = " + html);
-                $('#issue_description').val(template.issue_template.description);
-                $('#issue_subject').val(template.issue_template.issue_title);
-            });
-    }
+function load_template(target_url, token) {
+    if (template == "") {
+    } else $.ajax({
+        url:target_url,
+        async:true,
+        type:'post',
+        data:$.param({issue_tracker_id:$("#issue_template").val(), authenticity_token:token})
+    }).done(function (html) {
+            eval("var template = " + html);
+            $("#issue_description").val(template.issue_template.description);
+            $("#issue_subject").val(template.issue_template.issue_title);
+        });
 }
 
-function set_pulldown(tracker, target_url, token) {
+function set_pulldown(target_url, token) {
     $.ajax({
         url: target_url,
         async: true,
-        data:"issue_tracker_id=" + encodeURIComponent(tracker) + '&authenticity_token='
-            + encodeURIComponent(token)
+        type: 'post',
+        data: $.param({issue_tracker_id: $("#issue_tracker_id").val(), authenticity_token: token})
     }).done(function( html ) {
             $('#issue_template').html(html);
     });
