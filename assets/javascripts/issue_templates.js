c8aae3f0047b97986b37318b6e4133a3e34d5e4d
akiko-pusu <akiko.pusu@gmail.com>
2012-12-02 06:21:13 +0900

    Change the way to refresh template pulldown, because loading issue form was changed on Redmine2.1.x.  (IssueID: #1131) Change to insert template text just after  the text that is already in the description field. (IssueID: #1115)

--- assets/javascripts/issue_templates.js
+++ assets/javascripts/issue_templates.js
@@ -13,27 +13,41 @@ $(document).ready(function() {
     $('#template_area').insertBefore($('#issue_subject').parent());
 });
 
-function load_template(target_url, token) {
-    if (template == "") {
-    } else $.ajax({
-        url:target_url,
-        async:true,
-        type:'post',
-        data:$.param({issue_tracker_id:$("#issue_template").val(), authenticity_token:token})
-    }).done(function (html) {
-            eval("var template = " + html);
-            $("#issue_description").val(template.issue_template.description);
-            $("#issue_subject").val(template.issue_template.issue_title);
-        });
+// TODO: When update description, confirmation dialog sould be appeared.
+function load_template(target_url, token, confirm_msg) {
+    var allow_overwrite = $('#allow_overwrite_description').prop('checked');
+    if ($("#issue_template").val() != "") {
+        $.ajax({
+            url:target_url,
+            async:true,
+            type:'post',
+            data:$.param({issue_template:$("#issue_template").val(), authenticity_token:token})
+        }).done(function (html) {
+                oldSubj = "";
+                oldVal = "";
+                eval('var template = ' + html);
+                if ($("#issue_description").val() != '') {
+                    oldVal = $("#issue_description").val() + "\n\n";
+                }
+
+                if ($("#issue_subject").val() != '') {
+                    oldSubj = $("#issue_subject").val() + ' ';
+                }
+                $("#issue_description").val(oldVal + template.issue_template.description);
+                $("#issue_subject").val(oldSubj + template.issue_template.issue_title);
+            });
+    }
 }
 
-function set_pulldown(target_url, token) {
+function set_pulldown(tracker, target_url, token) {
+    var allow_overwrite = $('#allow_overwrite_description').prop('checked');
     $.ajax({
         url: target_url,
         async: true,
         type: 'post',
-        data: $.param({issue_tracker_id: $("#issue_tracker_id").val(), authenticity_token: token})
+        data: $.param({issue_tracker_id: tracker, authenticity_token: token})
     }).done(function( html ) {
-            $('#issue_template').html(html);
+        $('#issue_template').html(html);
+        $('#allow_overwrite_description').attr("checked", allow_overwrite);
     });
 }
