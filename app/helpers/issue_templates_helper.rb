2897c547af4c18521ce80283835e64346109e573
Akiko Takano <akiko.pusu@gmail.com>
2013-09-04 10:16:28 +0900

    Add feature to show warning message for orphaned templates. (IssueID: #1278)

--- app/helpers/issue_templates_helper.rb
+++ app/helpers/issue_templates_helper.rb
@@ -1,2 +1,20 @@
-module IssueTemplatesHelper 
+module IssueTemplatesHelper
+  def is_project_tracker(tracker_id, project)
+    if project.trackers.exists?(tracker_id)
+      return true
+    end
+    return false
+  end
+
+  def non_project_tracker_msg(flag)
+    return "" if flag
+    return "<font class=\"non_project_tracker\">#{l(:unused_tracker_at_this_project)}</font>".html_safe
+  end
+
+  def template_target_trackers(project, issue_template)
+    trackers = nil
+    trackers = project.trackers | [issue_template.tracker]
+    trackers = trackers.collect {|t| [t.name, t.id]}
+    return trackers
+  end
 end
