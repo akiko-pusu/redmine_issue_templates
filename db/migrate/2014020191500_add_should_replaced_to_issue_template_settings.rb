5120b99110c153372751aa1e6b97c96e2237d826
Akiko Takano <akiko.pusu@gmail.com>
2014-02-19 16:20:19 +0900

    Accept null value.

--- db/migrate/2014020191500_add_should_replaced_to_issue_template_settings.rb
+++ db/migrate/2014020191500_add_should_replaced_to_issue_template_settings.rb
@@ -1,6 +1,6 @@
 class AddShouldReplacedToIssueTemplateSettings < ActiveRecord::Migration
   def self.up
-    add_column :issue_template_settings, :should_replaced, :boolean, :default => false, :null => false
+    add_column :issue_template_settings, :should_replaced, :boolean, :default => false
   end
 
   def self.down
