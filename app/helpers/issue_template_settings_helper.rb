e6ca9b4445df2a5055930fe0122e007316ec8ce8
Akiko Takano <akiko.pusu@gmail.com>
2016-06-16 07:42:47 +0900

    Remove unused helper. Change to single quote.

--- app/helpers/issue_template_settings_helper.rb
+++
@@ -1,2 +0,0 @@
-module IssueTemplateSettingsHelper
-end
--- db/migrate/0004_add_position_to_issue_templates.rb
+++ db/migrate/0004_add_position_to_issue_templates.rb
@@ -5,7 +5,7 @@ class AddPositionToIssueTemplates < ActiveRecord::Migration
     IssueTemplate.reset_column_information
 
     issue_templates = IssueTemplate.all
-    say_with_time("Update each template to set default position.") do
+    say_with_time('Update each template to set default position.') do
       issue_templates.each_with_index { |t, i| t.update_attribute(:position, i + 1) }
     end
   end
