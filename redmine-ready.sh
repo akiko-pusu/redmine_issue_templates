3eaed59cdf78d30610999b2e0f7b653eda34f373
Akiko Takano <akiko.pusu@gmail.com>
2016-06-22 06:53:37 +0900

    Update shell script for build.

--- redmine-ready.sh
+++ redmine-ready.sh
@@ -9,11 +9,13 @@ test:
 HERE
 
 mkdir -p redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}
+shopt -s dotglob
 cp -r source/* redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/
 
 cd source
 ls -a | grep -v -E 'redmine-ready\.sh|wercker\.yml' | xargs rm -rf
 
+shopt -s dotglob
 mv ../redmine-${REDMINE_VERSION}/* ./
 gem install simplecov simplecov-rcov yard
 gem update bundler
