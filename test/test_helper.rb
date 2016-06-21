ade1200645c89ebfb24d2faf69393554fe024d5b
Akiko Takano <akiko.pusu@gmail.com>
2016-06-22 07:32:37 +0900

    Filter to report coverage this plugin only. Add wercker setting.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'codeclimate-test-reporter'
 require 'shoulda'
 if ENV['JENKINS'] == 'true'
   SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  true
 else
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
       SimpleCov::Formatter::HTMLFormatter,
@@ -12,10 +13,15 @@ else
 end
 
 CodeClimate::TestReporter.configure do |config|
-  config.git_dir = "#{File.dirname(__FILE__)}/../" 
+  config.git_dir = "#{Dir.pwd}/plugins/redmine_issue_templates"
 end
 
-SimpleCov.start
+SimpleCov.start do
+  add_filter do |source_file|
+    # report this plugin only.
+    !source_file.filename.include?('plugins/redmine_issue_templates') || !source_file.filename.end_with?(".rb")
+  end
+end
 
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/',
