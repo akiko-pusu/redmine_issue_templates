302903048ca341890cc937ae808fca3d3731e3db
Akiko Takano <akiko.pusu@gmail.com>
2016-10-19 01:23:04 +0900

    :bug: change the way to install phantomJS.

--- install-phantomjs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /tmp
-wget https://github.com/paladox/phantomjs/releases/download/2.1.7/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
