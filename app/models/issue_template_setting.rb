66b3fbaa68d5b5e8354a9c6de45d46a7827ef6ee
Akiko Takano <akiko.pusu@gmail.com>
2016-11-30 08:29:37 +0900

    :sparkles: Add rake task to apply/unapply inherit templates setting to/from child projects.

--- app/models/issue_template_setting.rb
+++ app/models/issue_template_setting.rb
@@ -1,4 +1,19 @@
 class IssueTemplateSetting < ActiveRecord::Base
+  #
+  # Class method
+  #
+  class << self
+    def apply_template_to_child_projects(project_id:)
+      setting = IssueTemplateSetting.find(project_id)
+      setting.apply_template_to_child_projects
+    end
+
+    def unapply_template_from_child_projects(project_id:)
+      setting = IssueTemplateSetting.find(project_id)
+      setting.unapply_template_from_child_projects
+    end
+  end
+
   include Redmine::SafeAttributes
   unloadable
   belongs_to :project
@@ -39,6 +54,16 @@ class IssueTemplateSetting < ActiveRecord::Base
 
   def unapply_template_from_child_projects
     update_inherit_template_of_child_projects(false)
+  end
+
+  def get_inherit_templates(tracker = nil)
+    return [] unless enabled_inherit_templates?
+
+    project_ids = project.ancestors.collect(&:id)
+    tracker = project.trackers.pluck(:tracker_id) if tracker.blank?
+
+    # first: get inherit_templates
+    IssueTemplate.get_inherit_templates(project_ids, tracker)
   end
 
   private
---
+++ spec/models/issue_template_setting_spec.rb
@@ -0,0 +1,89 @@
+require_relative '../spec_helper'
+
+describe IssueTemplateSetting do
+  let(:project) { FactoryGirl.create(:project) }
+  let(:independent_projects) { FactoryGirl.create_list(:project, 2) }
+  let(:subject) do
+    FactoryGirl.create(:issue_template_setting,
+                       project_id: project.id)
+  end
+  let(:child_projects) { Project.where(parent_id: project.id) }
+
+  describe '#child_projects' do
+    context 'no child projects' do
+      it 'return zero count' do
+        expect(subject.child_projects.count).to eq 0
+      end
+    end
+
+    context 'has child projects' do
+      before do
+        FactoryGirl.create_list(:project, 2, parent_id: project.id)
+      end
+      it 'return right number of all the child projects' do
+        expect(subject.child_projects.count).to eq 2
+      end
+
+      context 'has descendent projects' do
+        before do
+          FactoryGirl.create_list(:project, 2, parent_id: child_projects.last.id)
+        end
+
+        it 'return right number of all the descendent projects' do
+          expect(subject.child_projects.count).to eq 4
+        end
+
+        it('return zero count after breaking off relationship') do
+          expect(subject.child_projects.count).to eq 4
+          child_projects.each do |c|
+            c.set_parent!(nil)
+          end
+          subject.project.reload
+          expect(subject.child_projects.count).to eq 0
+        end
+      end
+    end
+  end
+
+  describe '#apply_template_to_child_projects' do
+    let(:tracker) { FactoryGirl.create(:tracker, :with_default_status) }
+    let!(:enabled_module) { FactoryGirl.create(:enabled_module, project_id: project.id) }
+    let!(:issue_templates) do
+      FactoryGirl.create_list(:issue_template, 4, project_id: project.id, tracker_id: tracker.id, enabled_sharing: true)
+    end
+    let(:child_project_template_settings) do
+      IssueTemplateSetting.where(project_id: child_projects.ids)
+    end
+
+    before do
+      FactoryGirl.create_list(:project, 4, parent_id: project.id)
+      # change to enabled issue template module
+      child_projects.each do |c|
+        FactoryGirl.create(:enabled_module, project_id: c.id)
+        IssueTemplateSetting.find_or_create(c.id)
+        c.trackers = [tracker]
+        c.save
+      end
+      subject.apply_template_to_child_projects
+    end
+
+    it "child project inhetits parent's template" do
+      expect(child_project_template_settings.first.inherit_templates).to be_truthy
+    end
+
+    it 'child project can use inherit template' do
+      expect(child_project_template_settings.first.get_inherit_templates.count).to eq 4
+    end
+
+    it 'All the child projects setting should be true' do
+      expect(child_project_template_settings.pluck(:inherit_templates).include?(false)).to be_falsey
+    end
+
+    it 'All the child projects setting should be false after unapplied' do
+      subject.unapply_template_from_child_projects
+      values_inherit_templates = child_project_template_settings.pluck(:inherit_templates)
+      expect(values_inherit_templates.include?(false)).to be_truthy
+      expect(values_inherit_templates.uniq.count).to eq 1
+    end
+  end
+end
