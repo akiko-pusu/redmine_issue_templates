be2e2b3174f8687146c973c58d487a4e16defb33
Akiko Takano <akiko.pusu@gmail.com>
2017-03-26 14:44:21 +0900

    :pencil2: Add own helper to generate options list for template pulldown.

--- app/helpers/issue_templates_helper.rb
+++ app/helpers/issue_templates_helper.rb
@@ -15,4 +15,15 @@ module IssueTemplatesHelper
     trackers = trackers.collect { |obj| [obj.name, obj.id] }
     trackers
   end
+
+  def options_for_template_pulldown(options)
+    options.map do |option|
+      html_attributes = {}
+      html_attributes[:selected] = option[:selected]
+      html_attributes[:value] = option[:value]
+      html_attributes[:class] = option[:class] if option[:class].present?
+      text = option[:name]
+      content_tag_string(:option, text, html_attributes)
+    end.join("\n").html_safe
+  end
 end
