dd0e4531f3505babc41d8ef86461908124fbde2e
Akiko Takano <akiko.pusu@gmail.com>
2017-04-01 20:18:05 +0900

    Fix for new issue template.

--- app/controllers/global_issue_templates_controller.rb
+++ app/controllers/global_issue_templates_controller.rb
@@ -48,7 +48,7 @@ class GlobalIssueTemplatesController < ApplicationController
     end
 
     render(layout: !request.xhr?,
-           locals: { checklist_enabled: checklist_enabled, trackers: trackers, apply_global_templates: apply_all_projects?,
+           locals: { checklist_enabled: checklist_enabled, trackers: trackers, apply_all_projects: apply_all_projects?,
                      issue_template: @global_issue_template, projects: projects }) && return
   end
 
