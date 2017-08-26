e89e40e6bf40cd90a8478609e3881485fe711168
Akiko Takano <akiko.pusu@gmail.com>
2017-08-26 22:26:19 +0900

    Update test code. (Because modal header was changed from "Issue Template" to "Template".)

--- spec/features/issue_template_spec.rb
+++ spec/features/issue_template_spec.rb
@@ -106,7 +106,7 @@ feature 'IssueTemplate', js: true do
       end
 
       scenario 'popup Template filter' do
-        expect(titlebar).to have_content('Issue template: Bug')
+        expect(titlebar).to have_content('Template: Bug')
       end
 
       context 'Template filtered' do
@@ -121,7 +121,7 @@ feature 'IssueTemplate', js: true do
         end
 
         scenario 'Click filtered link and applied template' do
-          table.find('tbody > tr > td:nth-child(5) > a').click
+          table.find('tbody > tr > td:nth-child(4) > a').click
           sleep(0.2)
           description = page.find('#issue_description')
           expect(description.value).to match 'Sample description for rspec'
@@ -162,7 +162,7 @@ feature 'IssueTemplate', js: true do
         issue_description.set(expected_description)
         page.find('#link_template_dialog').click
         sleep(0.2)
-        table.find('tbody > tr > td:nth-child(5) > a').click
+        table.find('tbody > tr > td:nth-child(4) > a').click
         sleep(0.2)
         modal_close.click
       end
@@ -179,7 +179,7 @@ feature 'IssueTemplate', js: true do
         issue_description.set('different description')
         page.find('#link_template_dialog').click
         sleep(0.2)
-        table.find('tbody > tr > td:nth-child(5) > a').click
+        table.find('tbody > tr > td:nth-child(4) > a').click
         sleep(0.2)
         modal_close.click
       end
