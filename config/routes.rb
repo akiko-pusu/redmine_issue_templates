5fe2b2bdcf4a3a6ac079683b9ef74ea17c407bcc
Akiko Takano <akiko.pusu@gmail.com>
2017-12-07 07:30:12 +0900

    Code refactoring. Change routing setting for issue template settings, to use resource base setting.

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,18 @@ Rails.application.routes.draw do
   get 'projects/:project_id/issue_templates', to: 'issue_templates#index'
   match 'projects/:project_id/issue_templates/:action', controller: 'issue_templates', via: %i[get post patch put]
   match 'projects/:project_id/issue_templates/:action/:id', to: 'issue_templates#edit', via: %i[patch put post get], as: 'issue_template'
-  match 'projects/:project_id/issue_templates_settings/:action', controller: 'issue_templates_settings', via: %i[get post patch put]
   match 'issue_templates/preview', to: 'issue_templates#preview', via: %i[get post]
-  match 'projects/:project_id/issue_templates_settings/preview', to: 'issue_templates_settings#preview', via: %i[get post]
   get 'projects/:project_id/issue_templates/orphaned_templates', to: 'issue_templates#orphaned_templates', as: 'project_orphaned_templates'
   resources :global_issue_templates, except: [:edit] do
     get 'preview', on: :collection
     get 'orphaned_templates', on: :collection
+  end
+
+  # for project issue template
+  resources :projects do
+    resources :issue_templates_settings, only: [] do
+      patch 'edit', on: :collection
+      post 'preview', on: :collection
+    end
   end
 end
