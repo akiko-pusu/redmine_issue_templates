4f8c40e2cd4282a28c435e6fad5dfdaa33a0b73d
Akiko Takano <akiko.pusu@gmail.com>
2017-12-09 14:57:02 +0900

    Change routing, prevent customized count to be over 3.

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,28 @@
 #
 Rails.application.routes.draw do
   concern :tamplate_common do
-    post 'preview', on: :collection
     get 'orphaned_templates', on: :collection
   end
 
-  resources :global_issue_templates, except: [:edit], concerns: :tamplate_common
+  concern :previewable do
+    post 'preview', on: :collection
+  end
+
+  resources :global_issue_templates, except: [:edit], concerns: %i[tamplate_common previewable]
 
   # for project issue template
   resources :projects, only: [] do
-    resources :issue_templates, except: [:edit], concerns: :tamplate_common do
+    resources :issue_templates, except: [:edit], concerns: [:tamplate_common] do
       post 'set_pulldown', on: :collection
       get 'list_templates', on: :collection
-      post 'load', on: :collection
     end
 
-    resources :issue_templates_settings, only: [] do
+    resources :issue_templates_settings, only: [], concerns: [:previewable] do
       patch 'edit', on: :collection
-      post 'preview', on: :collection
     end
+  end
+
+  resources :issue_templates, only: %i[load preview], concerns: [:previewable] do
+    post 'load', on: :collection
   end
 end
