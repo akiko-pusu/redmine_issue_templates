695469e105d0961e3152c93f7c01b37a28d72498
Akiko Takano <akiko.pusu@gmail.com>
2018-04-03 21:51:07 +0900

    Remove unused route.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get 'projects/:project_id/issue_templates', to: 'issue_templates#index'
   match 'projects/:project_id/issue_templates/:action', controller: 'issue_templates', via: [:get, :post, :patch, :put]
   match 'projects/:project_id/issue_templates/:action/:id', to: 'issue_templates#edit', via: [:patch, :put, :post, :get]
-  match 'projects/:project_id/issue_templates/move/:id', to: 'issue_templates#move_to', via: [:get, :post, :patch, :put]
   match 'projects/:project_id/issue_templates_settings/:action', controller: 'issue_templates_settings', via: [:get, :post, :patch, :put]
   match 'issue_templates/preview', to: 'issue_templates#preview', via: [:get, :post]
   match 'projects/:project_id/issue_templates_settings/preview', to: 'issue_templates_settings#preview', via: [:get, :post]
