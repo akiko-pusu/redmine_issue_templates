28ba9d82e1cdb0287f574be160697cd51afe9aec
Akiko Takano <akiko.pusu@gmail.com>
2018-05-27 17:23:48 +0900

    Remove cop disable

--- app/helpers/issue_templates_helper.rb
+++ app/helpers/issue_templates_helper.rb
@@ -45,7 +45,7 @@ module IssueTemplatesHelper
       send(name, *args)
     end
   end
-  # rubocop:enable Lint/ShadowingOuterLocalVariable, Style/MethodMissing
+  # rubocop:enable Lint/ShadowingOuterLocalVariable
 
   def respond_to_missing?(method_name, include_private = false)
     method_name.to_s == 'reorder_handle' || super
