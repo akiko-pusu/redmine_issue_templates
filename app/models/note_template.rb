3c02a1db75251405e7defb00a6a42bac1ac3f0bc
Akiko Takano <akiko.pusu@gmail.com>
2019-03-04 01:59:57 +0900

    Change validation field.

--- app/models/note_template.rb
+++ app/models/note_template.rb
@@ -2,15 +2,15 @@ class NoteTemplate < ActiveRecord::Base
   include Redmine::SafeAttributes
 
   # author and project should be stable.
-  safe_attributes 'title', 'description', 'enabled'
+  safe_attributes 'name', 'description', 'enabled', 'memo'
 
   belongs_to :project
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :tracker
 
   validates :project_id, presence: true
-  validates_uniqueness_of :title, scope: :project_id
-  validates :title, presence: true
+  validates_uniqueness_of :name, scope: :project_id
+  validates :name, presence: true
 
   scope :enabled, -> { where(enabled: true) }
   scope :search_by_tracker, lambda { |tracker_id|
