cb17f1a9e436f4e3ca4dcb735b38177d2fdb0750
Akiko Takano <akiko.pusu@gmail.com>
2019-03-09 11:07:18 +0900

    Remove duplicate lines.

--- app/controllers/issue_templates_controller.rb
+++ app/controllers/issue_templates_controller.rb
@@ -3,12 +3,9 @@ class IssueTemplatesController < ApplicationController
   layout 'base'
   helper :issues
   include Concerns::IssueTemplatesCommon
+  include Concerns::ProjectTemplatesCommon
   menu_item :issues
-
-  before_action :find_object, only: %i[show edit update destroy]
-  before_action :find_user, :find_project, :authorize, except: [:preview]
   before_action :find_tracker, :find_templates, only: %i[set_pulldown list_templates]
-  accept_api_auth :index, :list_templates, :load
 
   def index
     project_id = @project.id
@@ -37,10 +34,6 @@ class IssueTemplatesController < ApplicationController
         render formats: :json, locals: { project_templates: project_templates }
       end
     end
-  end
-
-  def show
-    render render_form_params
   end
 
   def new
@@ -152,23 +145,9 @@ class IssueTemplatesController < ApplicationController
     IssueTemplate.orphaned(@project.id)
   end
 
-  def find_user
-    @user = User.current
-  end
-
-  def find_tracker
-    @tracker = Tracker.find(params[:issue_tracker_id])
-  end
-
   def find_object
     @issue_template = IssueTemplate.find(params[:id])
     @project = @issue_template.project
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  end
-
-  def find_project
-    @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
@@ -179,25 +158,8 @@ class IssueTemplatesController < ApplicationController
     @global_templates = global_templates(@tracker.id)
   end
 
-  def save_and_flash(message, action_on_failure)
-    unless @issue_template.save
-      render render_form_params.merge(action: action_on_failure)
-      return
-    end
-
-    respond_to do |format|
-      format.html do
-        flash[:notice] = l(message)
-        redirect_to action: 'show', id: @issue_template.id, project_id: @project
-      end
-      format.js { head 200 }
-    end
-  end
-
-  def render_form_params
-    { layout: !request.xhr?,
-      locals: { checklist_enabled: checklist_enabled?,
-                issue_template: @issue_template, project: @project } }
+  def template
+    @issue_template
   end
 
   def setting
--- app/controllers/note_templates_controller.rb
+++ app/controllers/note_templates_controller.rb
@@ -1,10 +1,8 @@
 class NoteTemplatesController < ApplicationController
+  include Concerns::ProjectTemplatesCommon
   layout 'base'
   helper :issue_templates
   menu_item :issues
-  before_action :find_object, only: %i[show update destroy]
-  before_action :find_user, :find_project, :authorize, except: %i[preview load]
-  accept_api_auth :index, :load
 
   def index
     project_id = @project.id
@@ -41,10 +39,6 @@ class NoteTemplatesController < ApplicationController
     save_and_flash(:notice_successful_create, :new) && return
   end
 
-  def show
-    render render_form_params
-  end
-
   def update
     @note_template.safe_attributes = template_params
     save_and_flash(:notice_successful_update, :show)
@@ -73,36 +67,11 @@ class NoteTemplatesController < ApplicationController
     end
   end
 
-  def destroy
-    unless @note_template.destroy
-      flash[:error] = l(:enabled_template_cannot_destroy)
-      redirect_to action: :show, project_id: @project, id: @note_template
-      return
-    end
-
-    flash[:notice] = l(:notice_successful_delete)
-    redirect_to action: 'index', project_id: @project
-  end
-
   private
-
-  def find_user
-    @user = User.current
-  end
-
-  def find_tracker
-    @tracker = Tracker.find(params[:issue_tracker_id])
-  end
 
   def find_object
     @note_template = NoteTemplate.find(params[:id])
     @project = @note_template.project
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  end
-
-  def find_project
-    @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
@@ -112,23 +81,7 @@ class NoteTemplatesController < ApplicationController
                                           :name, :memo, :description, :enabled, :author_id, :position)
   end
 
-  def render_form_params
-    { layout: !request.xhr?,
-      locals: { note_template: @note_template, project: @project } }
-  end
-
-  def save_and_flash(message, action_on_failure)
-    unless @note_template.save
-      render render_form_params.merge(action: action_on_failure)
-      retur
-    end
-
-    respond_to do |format|
-      format.html do
-        flash[:notice] = l(message)
-        redirect_to action: 'show', id: @note_template.id, project_id: @project
-      end
-      format.js { head 200 }
-    end
+  def template
+    @note_template
   end
 end
