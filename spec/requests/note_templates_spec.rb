957d0808c58e01df02731e431a930a46bbc82833
Akiko Takano <akiko.pusu@gmail.com>
2019-03-11 21:13:09 +0900

    Fix spec failed.

--- spec/requests/note_templates_spec.rb
+++ spec/requests/note_templates_spec.rb
@@ -8,7 +8,7 @@ RSpec.configure do |c|
 end
 
 RSpec.describe 'Note Template', type: :request do
-  let(:user) { FactoryBot.create(:user, :password_same_login, login: 'manager', language: 'en', admin: false) }
+  let(:user) { FactoryBot.create(:user, :password_same_login, login: 'test-manager', language: 'en', admin: false) }
   let(:project) { FactoryBot.create(:project_with_enabled_modules) }
   let(:tracker) { FactoryBot.create(:tracker, :with_default_status) }
   let(:role) { FactoryBot.create(:role, :manager_role) }
