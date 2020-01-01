9380721ea5ed24e260ab158ff48dd28fee11f679
Akiko Takano <akiko.pusu@gmail.com>
2020-01-01 21:31:45 +0900

    Add column to store related (sample) link for the template.

---
+++ db/migrate/20200101204020_add_related_link_to_issue_templates.rb
@@ -0,0 +1,9 @@
+class AddRelatedLinkToIssueTemplates < ActiveRecord::Migration[5.2]
+  def self.up
+    add_column :issue_templates, :related_link, :text
+  end
+
+  def self.down
+    remove_column :issue_templates, :related_link
+  end
+end
---
+++ db/migrate/20200101204220_add_related_link_to_global_issue_templates.rb
@@ -0,0 +1,9 @@
+class AddRelatedLinkToGlobalIssueTemplates < ActiveRecord::Migration[5.2]
+  def self.up
+    add_column :global_issue_templates, :related_link, :text
+  end
+
+  def self.down
+    remove_column :global_issue_templates, :related_link
+  end
+end
